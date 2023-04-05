14599eb88e78a299aea66d9c09f8e8b9751d90fe
pruthivithejan <pruthivithejan.code@gmail.com>
2023-04-05 08:23:10 +0530

    Next.js, Sanity, TailwindCSS Starter

---
+++ schemas/index.ts
@@ -0,0 +1,7 @@
+import post from './post'
+import category from './category'
+import comment from './comment'
+import author from './author'
+import blockContent from './blockContent'
+
+export const schemaTypes = [post, category, comment, author, blockContent]
