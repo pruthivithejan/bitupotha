0ac4b9a7708a9a587629251c8ec58d84b43f510b
pruthivithejan <pruthivithejan.code@gmail.com>
2023-04-12 14:52:45 +0530

    Updated to Next.js 13.3

---
+++ content.ts
@@ -0,0 +1,10 @@
+// Team members
+export const people = [
+  {
+    name: "Pruthivi Thejan",
+    role: "Founder / CEO",
+    status: "Student, University of Sri Jayawardanapura",
+    imageUrl: "/team/pruthivithejan.jpg",
+  },
+  // More people...
+];
