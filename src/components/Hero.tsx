4334a8b60efd0bdfcbc00b7ee987a5a36f9a001c
pruthivithejan <pruthivithejan.code@gmail.com>
2023-04-17 14:01:58 +0530

    Customization 0.1

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,22 +28,20 @@ export default function Hero() {
             ශ්‍රී ලංකාවේ විශාලතම සිංහල තාක්ෂණික දැනුම සංග්‍රහය
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-            lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-            fugiat aliqua.
+            ලොව වටා නවතම තාක්ෂණික දැනුම සිංහලෙන් ඔබ වෙත...
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
-              href="#"
+              href="/blog"
               className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Get started
+              පිවිසෙන්න
             </a>
             <a
-              href="#"
+              href="/join"
               className="text-sm font-semibold leading-6 text-gray-900"
             >
-              Learn more <span aria-hidden="true">→</span>
+              එකතු වෙන්න <span aria-hidden="true">→</span>
             </a>
           </div>
         </div>
--- src/components/studio/Navbar.tsx
+++ src/components/studio/Navbar.tsx
@@ -10,9 +10,26 @@ const Navbar = (props: any) => {
           ආපසු වෙබ් අඩවියට
         </Link>
 
-        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-          අපගේ Discord සේවාදායකය හා සම්බන්ධ වන්න{" "}
-          <a href="#" className="font-semibold text-indigo-600">
+        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 inline">
+          අපගේ{" "}
+          <svg
+            className="w-5 h-5 inline"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M14.82 4.26a10.14 10.14 0 0 0-.53 1.1 14.66 14.66 0 0 0-4.58 0 10.14 10.14 0 0 0-.53-1.1 16 16 0 0 0-4.13 1.3 17.33 17.33 0 0 0-3 11.59 16.6 16.6 0 0 0 5.07 2.59A12.89 12.89 0 0 0 8.23 18a9.65 9.65 0 0 1-1.71-.83 3.39 3.39 0 0 0 .42-.33 11.66 11.66 0 0 0 10.12 0q.21.18.42.33a10.84 10.84 0 0 1-1.71.84 12.41 12.41 0 0 0 1.08 1.78 16.44 16.44 0 0 0 5.06-2.59 17.22 17.22 0 0 0-3-11.59 16.09 16.09 0 0 0-4.09-1.35zM8.68 14.81a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.93 1.93 0 0 1 1.8 2 1.93 1.93 0 0 1-1.8 2zm6.64 0a1.94 1.94 0 0 1-1.8-2 1.93 1.93 0 0 1 1.8-2 1.92 1.92 0 0 1 1.8 2 1.92 1.92 0 0 1-1.8 2z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          සේවාදායකය හා සම්බන්ධ වන්න{" "}
+          <a
+            href="https://discord.gg/dQxJhFFP"
+            className="font-semibold text-indigo-600"
+            target="_blank"
+          >
             <span className="absolute inset-0" aria-hidden="true" />
             මෙතැනින් <span aria-hidden="true">&rarr;</span>
           </a>
